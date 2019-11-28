ae4b87a0aafd769866a620b17b28fd4586181517
CG-SS <criguiss@uol.com.br>
2019-11-28 20:14:32 -0300

    Changing dead link
    The previous link was dead, the base URL changed to "http://cdimage.ubuntu.com/ubuntu-base/releases/16.04/release/"

--- 00_rootfs_build.sh
+++ 00_rootfs_build.sh
@@ -74,7 +74,7 @@ case $DISTRO in
 		ROOTFS="http://archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz"
 		;;
 	xenial)
-		ROOTFS="http://cdimage.ubuntu.com/ubuntu-base/xenial/daily/current/xenial-base-arm64.tar.gz"
+		ROOTFS="http://cdimage.ubuntu.com/ubuntu-base/releases/16.04/release/ubuntu-base-16.04-core-arm64.tar.gz"
 		;;
 	sid|jessie)
 		ROOTFS="${DISTRO}-base-arm64.tar.gz"
